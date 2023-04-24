ace484601a533dfcbdc7ee424f0685a8aec126c0
Firdosh01 <firdosh7392@gmail.com>
2023-04-24 07:35:15 -0500

    about section

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name="about" className='w-full h-screen text-white bg-gradient-to-b from-gray-800 to-black'>
+
+        <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto '>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold'>About</p>
+            </div>
+            <p className='text-sm '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis est illo, sapiente qui ipsum nobis? Aliquid veniam quos consequuntur ea corporis excepturi
+                 illum obcaecati omnis ducimus quaerat tenetur ipsam rem optio dolorum, amet accusantium repellendus rerum. Veniam numquam, ipsum est ipsam asperiores iure ipsa debitis laborum, cum dolor deleniti tenetur.</p>
+
+            <br />
+
+            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus recusandae eligendi fugit, expedita et magnam, tenetur optio quidem, necessitatibus excepturi doloremque at cumque modi 
+                incidunt officia impedit ipsa molestiae suscipit facere! Optio laborum deleniti libero culpa laudantium, quas natus voluptatem architecto nihil ab, ipsum amet omnis, molestias voluptas necessitatibus labore.</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
