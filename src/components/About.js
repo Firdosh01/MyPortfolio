0791c50a39ba9f13a0c965f523e647836aec5609
Firdosh01 <firdosh7392@gmail.com>
2023-04-24 22:52:44 -0500

    font size change

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,14 @@ function About() {
 
         <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto '>
             <div className='pb-8'>
-                <p className='text-4xl font-bold'>About</p>
+                <p className='inline text-4xl font-bold border-b-4 border-gray-500'>About</p>
             </div>
-            <p className='text-sm '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis est illo, sapiente qui ipsum nobis? Aliquid veniam quos consequuntur ea corporis excepturi
+            <p className='mt-20 text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis est illo, sapiente qui ipsum nobis? Aliquid veniam quos consequuntur ea corporis excepturi
                  illum obcaecati omnis ducimus quaerat tenetur ipsam rem optio dolorum, amet accusantium repellendus rerum. Veniam numquam, ipsum est ipsam asperiores iure ipsa debitis laborum, cum dolor deleniti tenetur.</p>
 
             <br />
 
-            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus recusandae eligendi fugit, expedita et magnam, tenetur optio quidem, necessitatibus excepturi doloremque at cumque modi 
+            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus recusandae eligendi fugit, expedita et magnam, tenetur optio quidem, necessitatibus excepturi doloremque at cumque modi 
                 incidunt officia impedit ipsa molestiae suscipit facere! Optio laborum deleniti libero culpa laudantium, quas natus voluptatem architecto nihil ab, ipsum amet omnis, molestias voluptas necessitatibus labore.</p>
         </div>
       
