00dedda67508afc8c49512b6d9fc26703a1b3597
Firdosh01 <firdosh7392@gmail.com>
2023-04-26 03:23:33 -0500

    Project Card styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,30 +4,52 @@ import TictactoeGame from '../assets/TictactoeGame.PNG'
 import DevDetective from '../assets/DevDetective.PNG'
 
 function Portfolio() {
+
+  const portfolios = [
+    {
+      id: 1,
+      src: Weather
+    },
+    {
+      id: 2,
+      src: TictactoeGame
+    },
+    {
+      id: 3,
+      src: DevDetective
+    },
+
+  ]
   return (
-    <div name= "Portfolio" className='w-full text-white bg-gradient-to-b from-black to-gray-800 md:h-screen'>
-      <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto'>
 
-        <div className='pb-8 mt-12'>
-        
-        <p className='inline text-4xl font-bold border-b-4 border-gray-500'>Portfolio</p>
-        <p className='py-6'>Check out some of my work right here</p>
-      
-      </div>
+      < div name = "Portfolio" className = 'w-full text-white bg-gradient-to-b from-black to-gray-800 md:h-screen' >
+        <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto'>
 
-      <div className='grid gap-8 px-12 sm:grid-cols-2 md:grid-cols-3 sm:px-0'>
-        <div className='rounded-lg shadow-md shadow-gray-600'>
+          <div className='pb-8 mt-12'>
 
-        <img src= {Weather} alt="" />
-        <div>
-          <button>Demo</button>
-          <button>Code</button>
+            <p className='inline text-4xl font-bold border-b-4 border-gray-500'>Portfolio</p>
+            <p className='py-6'>Check out some of my work right here</p>
+
+          </div>
+
+          <div className='grid gap-8 px-12 sm:grid-cols-2 md:grid-cols-3 sm:px-0'>
+            {
+              portfolios.map( ({id, src}) => (
+                <div key={id} className='rounded-lg shadow-md shadow-gray-600'>
+
+                  <img src={src} alt="" className='duration-200 rounded-md hover:scale-105' loading='lazy' />
+                  <div className='flex items-center justify-center'>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                  </div>
+                </div>
+
+              ))
+            }
+
+          </div>
+
         </div>
-        </div>
-
-      </div>
-
-    </div>
     </div>
   )
 }
