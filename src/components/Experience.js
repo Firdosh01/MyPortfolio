883538481434518282a09f719d1ac698941711f0
Firdosh01 <firdosh7392@gmail.com>
2023-08-23 00:20:41 -0500

    added skills

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,52 +1,8 @@
 import React from 'react'
-import html from '../assets/Experience/html.png'
-import css from '../assets/Experience/css.png'
-import tailwind from '../assets/Experience/tailwind.png'
-import javascript from '../assets/Experience/javascript.png'
-import react from '../assets/Experience/react.png'
-import github from '../assets/Experience/github.png'
-
+import { frontend, backend, otherskill} from './constants'
 
 function Experience() {
 
-    const techs = [
-        {
-            id: 1,
-            src: html,
-            title: 'HTML',
-            style: 'shadow-orange-500'
-        },
-        {
-            id: 2,
-            src: css,
-            title: 'CSS',
-            style: 'shadow-blue-500'
-        },
-        {
-            id: 3,
-            src: javascript,
-            title: 'Javascript',
-            style: 'shadow-yellow-500'
-        },
-        {
-            id: 4,
-            src: tailwind,
-            title: 'Tailwind',
-            style: 'shadow-blue-600'
-        },
-        {
-            id: 5,
-            src: react,
-            title: 'React',
-            style: 'shadow-sky-400'
-        },
-        {
-            id: 6,
-            src: github,
-            title: 'Github',
-            style: 'shadow-gray-400'
-        },
-    ]
     return (
         <div name="experience" className='w-full bg-gradient-to-b from-gray-800 to-black'>
 
@@ -54,14 +10,48 @@ function Experience() {
                 <div>
                     <p className='inline p-2 text-4xl font-bold border-b-4 border-gray-500'>Experience</p>
                     <p className='py-6'>These are the technologies I've worked with</p>
+                    <p className='inline p-2 text-md sharedSubTitle'>Front-End</p>
                 </div>
 
                 <div className='grid w-full grid-cols-2 gap-8 px-12 py-8 text-center sm:grid-cols-3 sm:px-0'>
                     {
-                        techs.map(({ id, src, title, style }) => (
+                        frontend.map(({ id, src, title, style }) => (
                             <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                                <img src={src} alt="" className='w-20 mx-auto' />
+                                <img src={src} alt={title} className='w-20 mx-auto mb-3' />
                                 <p> {title}</p>
+                            </div>
+
+                        ))
+                    }
+                </div>
+
+                <div>
+                    <p className='inline p-2 text-md sharedSubTitle'>Back-End</p>
+                </div>
+
+                <div className='grid w-full grid-cols-2 gap-8 px-12 py-8 text-center sm:grid-cols-3 sm:px-0'>
+                    {
+                        backend.map(({ id, src, title, style }) => (
+                            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                                <img src={src} alt={title} className='w-20 mx-auto mb-3' />
+                                <p> {title}</p>
+                            </div>
+
+                        ))
+                    }
+                </div>
+
+                <div>
+                    <p className='inline p-2 text-md sharedSubTitle'>Other-Softwares</p>
+                </div>
+
+                <div className='grid w-full grid-cols-2 gap-8 px-12 py-5 text-center sm:grid-cols-3 sm:px-0'>
+                    {
+                        otherskill.map(({ id, src, title, style, width }) => (
+                            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                                <img src={src} alt={title} className={`w-20 mx-auto mb-3  ${width}`} />
+                                <p> {title}</p>
+
                             </div>
 
                         ))
---
+++ src/components/constants.js
@@ -0,0 +1,93 @@
+// Skils 
+import html from '../assets/Experience/html.png'
+import css from '../assets/Experience/css.png'
+import tailwind from '../assets/Experience/tailwind.png'
+import javascript from '../assets/Experience/javascript.png'
+import react from '../assets/Experience/react.png'
+import github from '../assets/Experience/github.png'
+import nodejs from '../assets/Experience/nodejs.png'
+import mongoDB from '../assets/Experience/MongoDB.svg'
+import express from '../assets/Experience/Express.png'
+import postman from '../assets/Experience/postman.png'
+import vscode from '../assets/Experience/Vscode.png'
+
+// skills 
+export const frontend = [
+  {
+    id: 1,
+    src: html,
+    title: 'HTML',
+    style: 'shadow-orange-500'
+},
+{
+    id: 2,
+    src: css,
+    title: 'CSS',
+    style: 'shadow-blue-500'
+},
+{
+    id: 3,
+    src: javascript,
+    title: 'Javascript',
+    style: 'shadow-yellow-500'
+},
+{
+    id: 4,
+    src: tailwind,
+    title: 'Tailwind',
+    style: 'shadow-blue-600'
+},
+{
+    id: 5,
+    src: react,
+    title: 'React Js',
+    style: 'shadow-sky-400'
+},
+]
+
+export const backend = [
+    {
+        id: 1,
+        src: nodejs,
+        title: 'Node Js',
+        style: 'shadow-emerald-600'
+    },
+    {
+      id: 2,
+      src: express,
+      title: 'Express Js',
+      style: 'shadow-gray-400'
+    },
+    {
+        id: 3,
+        src: mongoDB,
+        title: 'MongoDb',
+        style: 'shadow-emerald-600'
+    },
+       
+]
+
+export const otherskill = [
+  {
+    id: 1,
+    src: github,
+    title: 'Github',
+    style: 'shadow-gray-400',
+   },
+   {
+    id: 2,
+    src: postman,
+    title: 'Postman',
+    style: 'shadow-orange-500',
+    width: 'w-[130px]'
+    },
+    {
+      id: 3,
+      src: vscode,
+      title: 'VS code',
+      style: 'shadow-sky-400',
+    },
+]
+
+
+
