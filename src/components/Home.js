9f546afaa5b19412f4505e6a7a234e4dd0623603
Firdosh01 <firdosh7392@gmail.com>
2023-08-23 00:39:03 -0500

    removed loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,35 @@ import React from 'react'
 import alexander from '../assets/alexander.jpg'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import { Link } from 'react-scroll';
+// import Typical from 'react-typical'
 
 function Home() {
   return (
     <div name='home' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 via-black'>
 
-      <div className='flex flex-col items-center justify-center h-full max-w-screen-lg px-4 mx-auto md:flex-row pt-36 '>
+      <div className='flex flex-col items-center justify-between h-full max-w-screen-lg px-4 pt-32 mx-auto md:flex-row '>
         <div className='flex flex-col justify-center h-full '>
           
-          <h2 className='text-4xl font-bold text-white sm:text-7xl'> I'm a Frontend Developer</h2>
-          <p className='max-w-md py-5 text-gray-500'>
-            I have 1 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            Tailwind, Javascript and React Js.
+          {/* <h2 className='text-4xl font-bold text-white sm:text-7xl'> I'm a Frontend Developer</h2> */}
+
+          <h2 className='text-3xl font-bold text-white sm:text-4xl'>Hello, I am</h2>
+          {/* <h2 className='text-[2rem] font-bold text-white sm:text-6xl'>
+          <Typical
+            loop={Infinity}
+            steps={[
+              'Frontend Developer',
+              8000, 
+              'Firdosh Ansari',
+              9000, 
+            ]}
+          />
+          </h2> */}
+            <h2 className='mt-1 text-4xl font-bold text-white typewriter sm:text-5xl'>
+              Firdosh Ansari
+            </h2>
+            
+          <p className='max-w-[35rem] py-2 text-gray-500'>
+             MERN stack web developer, specializing mostly in Frontend design and development.
           </p>
 
           <div>
@@ -28,7 +44,7 @@ function Home() {
         </div>
 
         <div>
-          <img src={alexander} alt="my profile" width={400} className='xsa:mt-3 rounded-2xl' />
+          <img src={alexander} alt="my profile" width={320} className='xsa:mt-3 rounded-2xl' loading='lazy' />
         </div>
         
       </div>
